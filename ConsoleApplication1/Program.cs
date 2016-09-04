6e56bc964e12f9349e0c7f3a881d314d46f8b906
blcol <blcol@LAPTOP-31HPT579>
2016-09-04 04:06:34 -0400

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treehouse.CodeChallenges
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            Console.Write("Enter the number of times to print \"Yay!\": ");
+            string entry = Console.ReadLine();
+
+            try
+            {
+                int number = Convert.ToInt32(entry);
+
+                for (int i = 0; i < number; i++)
+                {
+                    Console.WriteLine("Yay!");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("This is not valid input.");
+            }
+            
+                Console.WriteLine("You printed \"Yay!\" " + entry + " times!");
+            
+        }
+    }
+}
